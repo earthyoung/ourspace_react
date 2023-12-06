2b33c24165a1151ff424ef6116329111d9c648e8
earthyoung <studydatawithme@gmail.com>
2023-12-06 14:46:18 +0900

    feat: 기본 설정

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    const rawData = await fetch("http://127.0.0.1:8000/account/health", {mode: "no-cors"});
+    const jsonData = await rawData.json();
+    setData(jsonData);
+  };
+
+  useEffect(()=>{
+    getData();
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello
+      {data}
     </div>
   );
 }
