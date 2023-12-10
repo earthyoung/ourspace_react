247067acbc2c564403212fdb709c334c8a5c9f79
earthyoung <studydatawithme@gmail.com>
2023-12-10 23:21:46 +0900

    fix: app - react home, login, logout url만 유지

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Logout from './pages/Logout';
 import React, { useState } from 'react';
 
 export const LoginStateContext = React.createContext();
+export const dataStateContext = React.createContext();
 
 const App = () => {
 
   const [accessToken, setAccessToken] = useState("Default");
+  const [data, setData] = useState([]);
 
   return (
     <LoginStateContext.Provider value={{accessToken, setAccessToken}}>
-      <Router>
-            <Routes>
-              <Route path="/home" element={<Home />}></Route>
-              <Route path="/" element={<Login />}></Route>
-            </Routes>
-      </Router>
+      <dataStateContext.Provider value={{data, setData}}>
+        <Router>
+              <Routes>
+                <Route path="/" element={<Home />}></Route> 
+                <Route path="/login" element={<Login />}></Route>
+                <Route path="/logout" element={<Logout />}></Route>
+              </Routes>
+        </Router>
+      </dataStateContext.Provider>
     </LoginStateContext.Provider>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,53 +1,8 @@
-import { useContext, useEffect, useRef, useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { getAccessTokenGoogle, getAccessTokenKakao } from "./home_service"
-import { LoginStateContext } from "../App";
-
-
-
 const Home = () => {
-	const loginWith = useRef(localStorage.getItem("loginWith"));
-    const {accessToken, setAccessToken} = useContext(LoginStateContext);
-	const navigate = useNavigate();
-
-	useEffect(() => {
-		const queryString = window.location.search;
-		const urlParams = new URLSearchParams(queryString);
-		const codeParam = urlParams.get("code");
-		const accessToken = localStorage.getItem("accessToken");
-
-		if (codeParam && !accessToken && loginWith.current === "google") {
-			getAccessTokenGoogle(codeParam).then(res => {
-				localStorage.setItem("accessToken", res.access_token);
-                setAccessToken(res.access_token);
-			})
-		} else if (codeParam && !accessToken && loginWith.current === "kakao") {
-			getAccessTokenKakao(codeParam).then(res => {
-				localStorage.setItem("accessToken", res.access_token);
-                setAccessToken(res.access_token)
-			})
-		}
-	}, [loginWith])
-
-	// useEffect(() => {
-	// 	const accessToken = localStorage.getItem("accessToken")
-
-	// 	if (accessToken && loginWith.current === "Google") {
-	// 		getUserDataGoogle(accessToken).then(resp => {
-	// 			setUserDataGoogle(resp)
-	// 		})
-	// 	}
-	// }, [loginWith])
-
-	const setLogOut = () => {
-		localStorage.removeItem("accessToken")
-		localStorage.removeItem("loginWith")
-		navigate("/")
-	}
 
 	return (
 		<>
-            <h3>{accessToken}</h3>
+            <h3>This is Home</h3>
 		</>
 	)
 }
